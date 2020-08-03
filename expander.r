640b9ac0ef061171f9c0d54ef9811ee4c9468e84
abitamaspp <abitamasatria@gmail.com>
2020-08-03 11:43:30 +0700

    Full translate to English

--- expander.r
+++ expander.r
@@ -1,29 +1,39 @@
 #To encrypt:
 
 library(stringr)
-en <- readLines("[your_file_to_encrypt_here].txt")
-#file ngecoba.txt berada pada working directory, sehingga
-#perlu ditulis path directory secara lengkap
+en <- readLines("file_to_encrypt.txt")
+
+#if the file to encrypt not located at working directory,
+#you need to write its complete path directory
 
 en <- unlist(str_split(en, pattern = " "))
 en <- paste(paste(substr(en, 2, nchar(en)), 
             substr(en, 1, 1), "xyz", sep = ""),
             collapse = " ")
-write.table(en, file = "ngecobaa.txt", row.names = FALSE, 
+write.table(en, file = "encrypted.txt", row.names = FALSE, 
             col.names = FALSE, quote = FALSE)
-#argumen "file ="  pada fungsi write.table berfungsi untuk
-#memberikan nama file
-#apabila tidak menulis path directory, maka file ciphertext
-#secara default akan tersimpan di working directory
+#"file =" param used to name encrypted file/output file
+#If you don't write its complete path directory, then by default
+#it will be saved at your working directory
 
 
 
+
+#To decrypt:
   
-dec <- readLines("[your_file_to_decrypt_here].txt")
+dec <- readLines("file_to_decrypt.txt")
+
+#if the file to dencrypt not located at working directory,
+#you need to write its complete path directory
+
 dec <- unlist(str_split(dec, pattern = " "))
-dec <- gsub("$xxx", "", dec)
+dec <- gsub("$xyz", "", dec)
 dec <- paste(paste(substr(dec, nchar(dec), nchar(dec)), 
              substr(dec, 1, nchar(dec)-1), sep = ""), 
              collapse = " ")
-write.table(dec, file = "file_output.txt", row.names = FALSE,
+write.table(dec, file = "decrypted.txt", row.names = FALSE,
             col.names = FALSE, quote = FALSE)
+			
+#"file =" param used to name decrypted file/output file
+#If you don't write its complete path directory, then by default
+#it will be saved at your working directory
